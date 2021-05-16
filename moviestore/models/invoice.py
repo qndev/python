2cd638f6947128a35351708efa257b5149f2d2fc
Dinh Quang <quangnd.hust@gmail.com>
2021-05-17 03:33:36 +0700

    completed get invoice

---
+++ moviestore/models/invoice.py
@@ -0,0 +1,65 @@
+class Invoice:
+    def __init__(self, movie_name: str, category: str, release_month: str, days_rental: float, price: float, surcharge_new_movie: float, surcharge_days: float, discount: int, total_pay: float) -> None:
+        self._movie_name = movie_name
+        self._category = category
+        self._release_month = release_month
+        self._days_rental = days_rental
+        self._price = price
+        self._surcharge_new_movie = surcharge_new_movie
+        self._surcharge_days = surcharge_days
+        self._discount = discount
+        self._total_pay = total_pay
+
+    def get_movie_name(self):
+        return self._movie_name
+
+    def set_movie_name(self, movie_name):
+        self._movie_name = movie_name
+
+    def get_category(self):
+        return self._category
+
+    def set_category(self, category):
+        self._category = category
+
+    def get_release_month(self):
+        return self._release_month
+
+    def set_release_month(self, release_month):
+        self._release_month = release_month
+
+    def get_days_rental(self):
+        return self._days_rental
+
+    def set_days_rental(self, days_rental):
+        self._days_rental = days_rental
+
+    def get_price(self):
+        return self._price
+
+    def set_price(self, price):
+        self._price = price
+
+    def get_surcharge_new_movie(self):
+        return self._surcharge_new_movie
+
+    def set_surcharge_new_movie(self, surcharge_new_movie):
+        self._surcharge_new_movie = surcharge_new_movie
+
+    def get_surcharge_days(self):
+        return self._surcharge_days
+
+    def set_surcharge_days(self, surcharge_days):
+        self._surcharge_days = surcharge_days
+
+    def get_discount(self):
+        return self._discount
+
+    def set_discount(self, discount):
+        self._discount = discount
+
+    def get_total_pay(self):
+        return self._total_pay
+
+    def set_total_pay(self, total_pay):
+        self._total_pay = total_pay
--- moviestore/services/order_service.py
+++ moviestore/services/order_service.py
@@ -1,3 +1,6 @@
+import datetime
+import math
+from moviestore.models.invoice import Invoice
 from moviestore.constants.constant import Constants
 from typing import List
 from moviestore.models.order import Order
@@ -7,7 +10,7 @@ from moviestore.utils.file_utils import FileUltils
 class OrderService:
     def order_movies(self, order_items: List[str]) -> list:
 
-        return Order(None, None, None, None, None)
+        return Order(None, None, None)
 
     def export_invoice(self, customer_email) -> tuple:
         customer_info = FileUltils.read_customer_data(customer_email, False)
@@ -16,4 +19,94 @@ class OrderService:
         category_info = FileUltils.read_category_data()
         movies_info = FileUltils.read_order_movies_data(
             orders_info["movies"]["movie_ids"])
-        return customer_info, 0
+
+        invoice_data = []
+
+        for movie_id in movies_info:
+            invoice = Invoice()
+            invoice.set_movie_name(
+                movies_info[movie_id][Constants.MOVIE_KEYS[0]])
+            invoice.set_category(
+                category_info[movies_info[movie_id]["category_id"]]["name"])
+            invoice.set_release_month(
+                movies_info[movie_id][Constants.MOVIE_KEYS[2]])
+
+            days_rental = float((orders_info["movies"]["days_rental"])[
+                orders_info["movies"]["movie_ids"].index(movie_id)])
+
+            invoice.set_days_rental(days_rental)
+
+            category_id = category_info[movies_info[movie_id]["category_id"]]
+
+            price = float(category_id["price"])
+
+            order_date_str = orders_info["order_date"]
+
+            order_date = datetime.datetime.strptime(order_date_str, '%Y/%m/%d')
+
+            release_month_str = movies_info[movie_id]["release_month"]
+
+            release_month = datetime.datetime.strptime(
+                release_month_str, '%Y/%m')
+
+            surcharge_new_movie = 0
+
+            surcharge_days = 0
+
+            if (order_date - release_month < 6):
+                if (category_id == "CATE001"):
+                    if (days_rental > float(3)):
+                        surcharge_days = (days_rental > float(3))*1.5
+                    surcharge_new_movie = 1
+                if (category_id == "CATE002"):
+                    if (days_rental > float(5)):
+                        surcharge_days = (days_rental > float(5))*1.0
+                    surcharge_new_movie = 1
+                if (category_id == "CATE003"):
+                    if (days_rental > float(2)):
+                        surcharge_days = (days_rental > float(2))*3.0
+                    surcharge_new_movie = 3
+
+            invoice.set_surcharge_new_movie(surcharge_new_movie)
+
+            invoice.set_surcharge_days(surcharge_days)
+
+            total_price = price + surcharge_new_movie + surcharge_days
+
+            invoice.set_total_pay(total_price)
+
+            discount = 0
+
+            discount_points = int(customer_info["discount_points"])
+
+            if (discount_points > 200):
+                discount = 10
+                discount_extra_50 = math.ceil((discount_points - 200)/50)*1
+                discount = discount + discount_extra_50
+
+            if (discount > 20):
+                discount = 20
+
+            if (total_price > 500):
+                discount = 10
+
+            total_pay = discount*total_price
+
+            discount_points_after_pay = 0
+
+            if (total_pay >= 100):
+                discount_points_after_pay = 10
+                discount_points_after_pay = discount_points_after_pay + \
+                    math.ceil((total_pay - 100)*5)
+            invoice.set_total_pay(total_pay)
+
+            invoice.set_discount(discount)
+
+            invoice_data.append(invoice)
+
+            print("Invoice: ")
+            print(invoice)
+            print("Discount Points: ")
+            print(discount_points_after_pay)
+
+        return customer_info, invoice
